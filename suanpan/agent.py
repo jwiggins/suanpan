2e2a34f79b1bbdbb96baeb6be97ac30d9032f21e
John Wiggins <plasticjeebus@gmail.com>
2018-03-19 21:50:27 +0100

    Fix some small bugs

--- suanpan/agent.py
+++ suanpan/agent.py
@@ -10,15 +10,15 @@ def loop(recipients):
     row_id = 0
     while True:
         msg, sender, row_id = wait_for_next_message(recipients,
-                                                    last_row_id=row_id)
+                                                    last_rowid=row_id)
         print('Recieved message:', msg)
         send_message(sender, "Hi there! I'm a bot!")
 
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument('-r', '--show-recipients', action='store_true')
-    ap.add_argument('recipients', nargs='?')
+    ap.add_argument('-s', '--show-recipients', action='store_true')
+    ap.add_argument('-r', '--recipients', nargs='+')
     args = ap.parse_args()
 
     all_recips = get_all_recipients()
